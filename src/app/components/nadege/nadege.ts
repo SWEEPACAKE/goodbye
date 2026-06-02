cb3f9504e451f62b21355ddd5472be4e7e60236f
rainbowceleste8-universe <rainbowceleste8@gmail.com>
2026-06-02 10:37:38 +0200

    chatpush

--- src/app/components/nadege/nadege.ts
+++ src/app/components/nadege/nadege.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nadege',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './nadege.html',
-  styleUrl: './nadege.scss',
+  styleUrls: ['./nadege.scss'],
 })
 export class Nadege {
-
+  logoUrl = '/images/Wish_Cats_Surprised.webp';
+  farewellMessage = 'Merci pour tous ces moments informatiquement instructifs partagés.';
+  farewellDetails = [
+    'Que la suite soit rempli de réussite et de belles aventures technologiques pour vous tous.',
+    
+    'À bientôt !',
+    '🎀 Nadège 🎀',
+    '😸'
+  ];
 }
